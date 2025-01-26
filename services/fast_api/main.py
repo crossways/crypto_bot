d84f3a1c145d2629bcd7586a9a41207533b53b4f
Chris <nowakchristian26@gmx.de>
2025-01-26 18:53:34 +0100

    Changes to API to return ML predictions

--- services/fast_api/main.py
+++ services/fast_api/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Query
 from pathlib import Path
 from datetime import date
 from typing import Optional
+from joblib import load
 
 from src.config.config_loader import load_config
 from src.db.database_handler import connect_to_database
@@ -17,6 +18,10 @@ api = FastAPI(openapi_tags=[
     {
         'name': 'candlestick',
         'description': 'Candlestick Data. Open, High, Low, Close, Volume'
+    },
+    {
+        'name': 'predictions',
+        'description': 'Predicts next cloxing prices via ML'
     }
 ])
 
@@ -42,3 +47,14 @@ def get_candlesticks(symbol: str, target_interval: str, start_date: Optional[dat
     data = psql_ops.get_candlestick_data(conn, symbol, target_interval, chosen_interval, start_date)
 
     return data
+
+
+@api.get("/predict/{symbol}", tags=['predictions'])
+def predict(symbol: str, periods: int, freq: str, model_version: str='15m', history: bool=False):
+    loaded_model = load(f"/app/ml_models/prophet_model_{model_version}.joblib")
+    #loaded_model = load(f"../../ml_models/prophet_model_{model_version}.joblib")
+    future = loaded_model.make_future_dataframe(periods=periods, freq=freq, include_history=history)
+    forecast = loaded_model.predict(future)
+    f = forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']]
+
+    return f.to_dict(orient='records')
